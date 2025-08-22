c9c932e5723c4a3243164c901b41b64b73569b30
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-22 13:57:14 +0900

    Initial Commit

---
+++ Tamagochiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tamagochiz
+//
+//  Created by Lee on 8/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
