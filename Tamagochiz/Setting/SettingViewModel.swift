28da78fa32808abfe90a9043cbc54d01d0927d96
우다당탕 개발자 <uddt_ds@kakao.com>
2025-08-22 16:30:39 +0900

    [Refactor] RxViewModelProtocol 구현에 따른 SettingViewModel 적용

--- Tamagochiz/Setting/SettingViewModel.swift
+++ Tamagochiz/Setting/SettingViewModel.swift
@@ -9,7 +9,7 @@ import Foundation
 import RxSwift
 import RxCocoa
 
-final class SettingViewModel {
+final class SettingViewModel: RxViewModelProtocol {
 
     private var disposeBag = DisposeBag()
 
